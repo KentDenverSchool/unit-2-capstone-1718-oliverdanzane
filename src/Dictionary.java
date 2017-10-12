70940bec3ac80227051fb951b1c532483f9918ae
Dan <dantaro99@gmail.com>
2017-10-12 09:06:46 -0600

    added interface

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public interface Dictionary<Key, Value>{
+    //add an key-value pair to the dictionary
+    public void put(Key key, Value value);
 
-public class Dictionary {
+    //get the value associated with a given key
+    public Value get(Key key);
+
+    //remove a key-value pair and return its value
+    public Value remove(Key key);
+
+    //returns true if the dictionary is empty
+    public boolean isEmpty();
+
+    //returns the number of key-value pairs in the dictionary
+    public int size();
 }
